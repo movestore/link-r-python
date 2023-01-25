807468c18115605521bf39fa6a3bdda9d7575a05
Clemens Hahn <clemens@couchbits.com>
2023-01-25 12:14:25 +0100

    read env vars

--- python/csv_2_pickle.py
+++ python/csv_2_pickle.py
@@ -1,8 +1,9 @@
 import transform_to_pickle
+import os
 
 if __name__ == '__main__':
     transform_to_pickle.convert(
-        input_data_file_name='/tmp/artifacts/buffer.csv',
-        input_meta_file_name='/tmp/artifacts/meta.csv',
-        output_file_name='/tmp/output_file'
+        input_data_file_name=os.environ['LINK_R_PYTHON_BUFFER'] or '/tmp/artifacts/buffer.csv',
+        input_meta_file_name=os.environ['LINK_R_PYTHON_META'] or '/tmp/artifacts/meta.csv',
+        output_file_name=os.environ['OUTPUT_FILE']
     )
