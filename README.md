b90df10591080959e35670b4a041ee402f08ed8c
Clemens Hahn <clemens@couchbits.com>
2023-01-25 15:25:07 +0100

    fix the start script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MoveApps: Link R Python
+
+- https://towardsdatascience.com/conda-pip-and-docker-ftw-d64fe638dc45
--- r2python.sh
+++ r2python.sh
@@ -2,11 +2,18 @@
 ########################################################################################################################
 # calls the co-pilot-r SDK entry point
 ########################################################################################################################
-set -o errexit
-set -o pipefail
-set -o nounset
+#set -o errexit
+#set -o pipefail
+#set -o nounset
+# import for `conda activate`
+set -e
 
-conda activate "$HOME"/co-pilot-r/python-env
+#export SOURCE_FILE=/tmp/source_file
+#export CONFIGURATION='{}'
+#export OUTPUT_FILE=/tmp/go-home
 
-(cd r && Rscript r/rds_2_csv.R)
-python python/csv_2_pickle.py
+#echo "$PATH"
+#conda activate "$HOME"/co-pilot-r/python-env
+#echo "after $PATH"
+(cd r && Rscript rds_2_csv.R)
+(cd python && conda activate "$HOME"/co-pilot-r/python-env && python csv_2_pickle.py)
