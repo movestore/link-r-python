ae8b376e147c0ca09356357b60d192f9c640e229
Clemens Hahn <clemens@couchbits.com>
2023-01-25 09:09:35 +0100

    write metadata (including timezone) and error-handling

--- r/r2csv.R
+++ r/r2csv.R
@@ -1,10 +1,29 @@
+source("src/common/logger.R")
+source("src/common/runtime_configuration.R")
+source("src/io/app_files.R")
+source("src/io/io_handler.R")
+source("src/io/rds.R")
+
 library(move)
-data <- readRDS("/tmp/source_file") # .rds
 
-data.csv <- as.data.frame(data)
-names(data.csv) <- make.names(names(data.csv),allow_=FALSE)
-# use for geopandas: trackId, timestamps, location.lat and location.long (plus all the attributes as a table)
-write.csv(data.csv,"/tmp/artifacs/buffer.csv",row.names=FALSE) 
+tryCatch(
+    {
+        Sys.setenv(tz="UTC")
+        data <- readInput(sourceFile()) # .rds
 
-projection <- raster::projection(data) # for move2 this will be st_crs()
-cat(projection,file="/tmp/artifacs/projection.txt")
+        data.csv <- as.data.frame(data)
+        names(data.csv) <- make.names(names(data.csv),allow_=FALSE)
+        # use for geopandas: trackId, timestamps, location.lat and location.long (plus all the attributes as a table)
+        write.csv(data.csv, appArtifactPath("buffer.csv"),row.names=FALSE) 
+
+        projection <- raster::projection(data) # for move2 this will be st_crs()
+        tz <- attr(timestamps(data),'tzone')
+        write.csv(data.frame("crs"=c(pr), "tzone"=c(tz)),appArtifactPath("meta.csv"),row.names=FALSE) #or any other format
+    },
+    error = function(e)
+    {
+        # error handler picks up where error was generated
+        print(paste("ERROR: ", e))
+        storeToFile(e, errorFile())
+        stop(e) # re-throw the exception
+    })
