5afd08abc47ed435cbb7536b1b390df58d843786
Clemens Hahn <clemens@couchbits.com>
2023-01-25 14:23:02 +0100

    fix the python part

--- python/csv_2_pickle.py
+++ python/csv_2_pickle.py
@@ -2,8 +2,7 @@ from transform_to_pickle import TransformToPickle
 import os
 
 if __name__ == '__main__':
-    transformer = TransformToPickle()
-    transformer.transform_to_pickle.convert(
+    TransformToPickle().convert(
         input_data_file_name=os.environ.get('LINK_R_PYTHON_BUFFER', '/tmp/artifacts/buffer.csv'),
         input_meta_file_name=os.environ.get('LINK_R_PYTHON_META', '/tmp/artifacts/meta.csv'),
         output_file_name=os.environ['OUTPUT_FILE']
