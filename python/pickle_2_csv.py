f79b2412c98e28dfde95529e03995b0cc8a1698a
Clemens Hahn <clemens@couchbits.com>
2023-02-09 15:09:46 +0100

    provide meta file name

--- python/pickle_2_csv.py
+++ python/pickle_2_csv.py
@@ -4,5 +4,6 @@ import os
 if __name__ == '__main__':
     TransformToCsv().convert(
         input_data_file_name=os.environ['SOURCE_FILE'],
-        output_file_name=os.environ.get('LINK_R_PYTHON_BUFFER', '/tmp/artifacts/buffer.csv')
+        output_file_name=os.environ.get('LINK_R_PYTHON_BUFFER', '/tmp/artifacts/buffer.csv'),
+        output_meta_file_name=os.environ.get('LINK_R_PYTHON_META', '/tmp/artifacts/meta.csv')
     )
