e2d14b15abaf0c8c514b71d89fa206fdc434b4bf
Clemens Hahn <clemens@couchbits.com>
2023-02-09 08:42:13 +0100

    improve mapping from moveStack to CSV buffer

--- r/rds_2_csv.R
+++ r/rds_2_csv.R
@@ -8,17 +8,46 @@ library(move)
 
 tryCatch(
     {
-        Sys.setenv(tz="UTC")
-        data <- readInput(sourceFile()) # .rds
-
-        data.csv <- as.data.frame(data)
-        names(data.csv) <- make.names(names(data.csv),allow_=FALSE)
-        # use for geopandas: trackId, timestamps, location.lat and location.long (plus all the attributes as a table)
-        write.csv(data.csv, appArtifactPath("buffer.csv"),row.names=FALSE) 
-
-        projection <- raster::projection(data) # for move2 this will be st_crs()
-        tz <- attr(timestamps(data),'tzone')
-        write.csv(data.frame("crs"=c(projection), "tzone"=c(tz)),appArtifactPath("meta.csv"),row.names=FALSE) #or any other format
+      Sys.setenv(tz="UTC")
+      
+      data <- readInput(sourceFile()) # .rds
+      data.csv <- as.data.frame(data)
+      names(data.csv) <- make.names(names(data.csv),allow_=FALSE)
+      
+      # Remove apparently duplicated columns, but which are
+      # often not true duplicates. Just to avoid mistakes 
+      # that could mess all up and no one would understand why
+      data.csv$timestamps <- timestamps(data)
+      if("timestamp" %in% names(data.csv)){data.csv$timestamp <- NULL}
+      data.csv$location.long <- coordinates(data)[,1]
+      data.csv$location.lat <- coordinates(data)[,2]
+      if("coords.x1" %in% names(data.csv)){data.csv[,c("coords.x1","coords.x2")] <- NULL}
+      data.csv$sensor <- sensor(data)
+      if("sensor.type" %in% names(data.csv)){data.csv$sensor.type <- NULL}
+      
+      if (!"individual.local.identifier" %in% names(data.csv)){
+        if ("local.identifier" %in% names(data.csv)) {
+          names(data.csv)[which(names(data.csv)=="local.identifier")] <- "individual.local.identifier"
+        } else {
+          # keep the trackId column, for consistency
+          data.csv$individual.local.identifier <- data.csv$trackId}
+      } 
+      # if no local.identifier given, then use trackId (which is always there)
+      
+      if (!"individual.taxon.canonical.name" %in% names(data.csv)){
+        if ("taxon.canonical.name" %in% names(data.csv)) {
+          names(data.csv)[which(names(data.csv)=="taxon.canonical.name")] <- "individual.taxon.canonical.name"
+        } else {
+          data.csv$individual.taxon.canonical.name <- NA
+        }
+      }
+      # if no local.taxon given, then set NA (Not Available)
+      
+      write.csv(data.csv,appArtifactPath("buffer.csv"),row.names=FALSE)
+      
+      projection <- raster::projection(data) # for move2 this will be st_crs()
+      tz <- attr(timestamps(data),'tzone')
+      write.csv(data.frame("crs"=c(projection), "tzone"=c(tz)),appArtifactPath("meta.csv"),row.names=FALSE)
     },
     error = function(e)
     {
@@ -26,4 +55,5 @@ tryCatch(
         print(paste("ERROR: ", e))
         storeToFile(e, errorFile())
         stop(e) # re-throw the exception
-    })
+    }
+)
