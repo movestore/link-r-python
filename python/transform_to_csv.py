d35db369c6079e87b00bc70776ffc4170c99fc69
Clemens Hahn <clemens@couchbits.com>
2024-01-11 13:35:50 +0100

    transform `pickle` created by `move2` to `csv`

--- python/transform_to_csv.py
+++ python/transform_to_csv.py
@@ -31,34 +31,41 @@ class TransformToCsv:
             # this is untested for now
             return data.index.tz
 
-    def write_meta_csv(self, data: gpd.GeoDataFrame, file_path):
-        projection: CRS = self.get_projection(data=data)
-        tzone = self.get_timezone(data=data)
+    def get_time_col_name(self, data: gpd.GeoDataFrame) -> str:
+        return data.index.name
+
+    def get_track_id_col_name(self, data: mpd.TrajectoryCollection) -> str:
+        return data.get_traj_id_col()
+
+    def write_meta_csv(self, geopanda: gpd.GeoDataFrame, movingpanda: mpd.TrajectoryCollection, file_path):
+        projection: CRS = self.get_projection(data=geopanda)
+        tzone = self.get_timezone(data=geopanda)
+        time_col_name = self.get_time_col_name(data=geopanda)
+        track_id_col_name = self.get_track_id_col_name(data=movingpanda)
         meta = {
             "crs": [projection.srs],
-            "tzone": [tzone]#,
-            ## to be done: add names of column containing timestamps and track id
-            # "timeColName": [timeColName],
-            # "trackIdColName": [trackIdColName]
+            "tzone": [tzone],
+            "timeColName": [time_col_name],
+            "trackIdColName": [track_id_col_name]
         }
         df = pd.DataFrame(meta)
         df.to_csv(file_path, index=False)
 
     def write_result(self, file_name, data: gpd.GeoDataFrame):
         print(type(data))
-        data.to_csv(file_name, index=False)
+        data.to_csv(file_name, index=True)
 
     def convert(self, input_data_file_name, output_file_name, output_meta_file_name):
         data = self.read_data_pickle(file_path=input_data_file_name)
         geopandas = self.create_geopandas(data=data)
         self.write_result(file_name=output_file_name, data=geopandas)
-        self.write_meta_csv(data=geopandas, file_path=output_meta_file_name)
+        self.write_meta_csv(geopanda=geopandas, movingpanda=data, file_path=output_meta_file_name)
 
 
 # just for dev
 if __name__ == '__main__':
     TransformToCsv().convert(
         input_data_file_name='./sample/csv-to-pickle/out.pickle',
-        output_file_name='./sample/pickle-to-csv/link.csv',
+        output_file_name='sample/pickle-to-csv/link.csv',
         output_meta_file_name='./sample/pickle-to-csv/meta.csv'
     )
